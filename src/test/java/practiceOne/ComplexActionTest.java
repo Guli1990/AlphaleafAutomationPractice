1f5c82bc5438a0474a237fccc6c78eed73e2e469
Gulgina Abdusalam <gulgina@nijats-mbp.lan>
2021-03-23 20:07:37 -0400

    first project image

---
+++ src/test/java/practiceOne/ComplexActionTest.java
@@ -0,0 +1,10 @@
+package practiceOne;
+
+import org.testng.annotations.Test;
+
+public class ComplexActionTest {
+    @Test
+    public void hoverOverTest(){
+        System.out.println("Hello");
+    }
+}
